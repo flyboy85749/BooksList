46d4de3e2baf9a3cda640c488e07466729a55b7e
William Hall <billhall168@gmail.com>
2019-12-14 11:16:11 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Ruoter, Switch, Route } from Router;
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+app.get("/")
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route exact path="/books" component={Books} />
+          <Route exact path="/books/:id" component={Books} />
+          <Route exact path="*" component={NoMatch} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
